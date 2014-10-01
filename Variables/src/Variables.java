9f4acb4da47d04fcb9f8024a137feab6a7900934
Paco Gomez <fjgomez@florida-uni.es>
2014-10-01 13:13:48 +0200

    Inicio repositorio
    Signed-off-by: Paco Gomez <fjgomez@florida-uni.es>

---
+++ Variables/src/Variables.java
@@ -0,0 +1,50 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+
+
+public class Variables extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Variables ventana = new Variables();
+					ventana.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Variables() {
+		String cadenaTexto="Paco";
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 500);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JTextField cajaTexto = new JTextField();
+		cajaTexto.setBounds(10, 46, 292, 20);
+		contentPane.add(cajaTexto);
+		cajaTexto.setColumns(10);
+		
+		cajaTexto.setText(cadenaTexto);
+	}
+}
