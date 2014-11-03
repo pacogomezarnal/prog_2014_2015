bfabc9ea269f190ebf5efb0b196fb72b752e9448
pacogomezarnal <fjgomez@florida-uni.es>
2014-11-03 22:15:19 +0100

    Constructores
    Signed-off-by: pacogomezarnal <fjgomez@florida-uni.es>

---
+++ ConstructoresObjetos/src/Principal.java
@@ -0,0 +1,13 @@
+
+public class Principal {
+	//DEFINICION DE LOS OBJETOS
+	public static SegundaClase s1;
+	public static SegundaClase s2;
+	
+	public static void main(String[] args) {
+		//CREACION DE LOS OBJETOS
+		s1=new SegundaClase(3);
+		s2=new SegundaClase(6);
+	}
+
+}
---
+++ ConstructoresObjetos/src/SegundaClase.java
@@ -0,0 +1,14 @@
+
+public class SegundaClase {
+	//Variable
+	int num=0;
+	
+	public SegundaClase(int valor) {
+		System.out.println("El valor de num anter de guardar es "+num);
+		
+		num=valor;
+		
+		System.out.println("El valor de num anter de guardar es "+num);
+	}
+
+}
