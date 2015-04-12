86785113e42945ec87649c099b4b757c57582b52
pacogomezarnal <fjgomez@florida-uni.es>
2015-04-12 08:37:33 +0200

    Ejemplo sobre base de datos
    Signed-off-by: pacogomezarnal <fjgomez@florida-uni.es>

---
+++ CorredoresBBDD/src/Corredores.java
@@ -0,0 +1,20 @@
+
+public class Corredores {
+
+	public static void main(String[] args) {
+		ConexionDB corredoresDB;
+		modeloCorredores mCorredor;
+		
+		//Generado el objeto
+		corredoresDB=new ConexionDB("localhost","corredores","root","");
+
+		if(corredoresDB.connectDB()==true) {
+			System.out.println("CONECTADOS CON EXITO");
+			mCorredor=new modeloCorredores(corredoresDB.getConexion());
+			mCorredor.getDatosCorredores();
+			System.out.println("LISTADO DE CORRREDORES");
+		}
+		else System.out.println("ERROR EN LA CONEXION");
+	}
+
+}
